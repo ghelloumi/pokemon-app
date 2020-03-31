8b114fd590505bccafa28d0e685cd2ca81c3da17
Ghassen <ell.ghassen@gmail.com>
2020-03-31 04:04:01 +0200

    First Commit

--- src/components/molecules/PokemonCard.js
+++ src/components/molecules/PokemonCard.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Modal from "../atoms/Modal";
 import PokemonDetails from "../organisms/PokemonDetails";
 import Image from "../atoms/Image";
